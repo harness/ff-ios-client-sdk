99a403900009a7401753dfc96c579aa8738c3039
Dusan Juranovic <dusan.juranovic@harness.io>
2021-03-02 14:54:47 +0100

    Renamed .h file

---
+++ ff-ios-client-sdk/ff-ios-client-sdk.h
@@ -0,0 +1,18 @@
+//
+//  ff-ios-client-sdk.h
+//  ff-ios-client-sdk
+//
+//  Created by Dusan Juranovic on 13.1.21..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ff-ios-client-sdk.
+FOUNDATION_EXPORT double ff-ios-client-sdkVersionNumber;
+
+//! Project version string for ff-ios-client-sdk.
+FOUNDATION_EXPORT const unsigned char ff-ios-client-sdkVersionString[];
+#import <ff-ios-client-sdk/ff-ios-client-sdk.h>
+// In this header, you should import all the public headers of your framework using statements like #import <ff-ios-client-sdk/PublicHeader.h>
+
+
