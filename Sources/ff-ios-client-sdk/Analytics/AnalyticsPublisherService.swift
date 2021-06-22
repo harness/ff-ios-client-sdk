9b69a96ee0cdb1de7ed3170355abf5743b4c2a56
Milos Vasic <milos.vasic@harness.io>
2021-06-22 16:21:42 +0200

    FFM-909 - Cache init.

--- Sources/ff-ios-client-sdk/Analytics/AnalyticsPublisherService.swift
+++ Sources/ff-ios-client-sdk/Analytics/AnalyticsPublisherService.swift
@@ -7,17 +7,27 @@ class AnalyticsPublisherService {
     private let config: CfConfiguration
     private let cache: [Analytics:Int]
     
+    private static let CLIENT: String = "client"
+    private static let SDK_TYPE: String = "SDK_TYPE"
+    private static let SDK_VERSION: String = "SDK_VERSION"
+    private static let SDK_LANGUAGE: String = "SDK_LANGUAGE"
+    private static let GLOBAL_TARGET: String = "__global__cf_target"
+    private static let TARGET_ATTRIBUTE: String = "target"
+    private static let FEATURE_NAME_ATTRIBUTE: String = "featureName"
+    private static let VARIATION_IDENTIFIER_ATTRIBUTE: String = "variationIdentifier"
+    
     init(
     
         cluster: String,
         environmentID: String,
-        config: CfConfiguration
+        config: CfConfiguration,
+        cache: [Analytics:Int]
     
     ) {
         
         self.cluster = cluster
         self.environmentID = environmentID
         self.config = config
-        self.cache = [Analytics:Int]()
+        self.cache = cache
     }
 }
