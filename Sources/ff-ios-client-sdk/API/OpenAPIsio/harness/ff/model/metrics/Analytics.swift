d8a9b02155fa24b052521e15427a9b839a1f7a63
Milos Vasic <milos.vasic@harness.io>
2021-06-24 10:19:37 +0200

    FFM-909 - Small cleanup.

--- Sources/ff-ios-client-sdk/API/OpenAPIsio/harness/ff/model/metrics/Analytics.swift
+++ Sources/ff-ios-client-sdk/API/OpenAPIsio/harness/ff/model/metrics/Analytics.swift
@@ -1,17 +1,12 @@
 import Foundation
 
-public struct Analytics : Codable, Hashable, Equatable {
+public struct Analytics : Codable, Hashable {
     
     public func hash(into hasher: inout Hasher) {
         
         hasher.combine(featureConfig)
     }
-    
-    public static func == (lhs: Analytics, rhs: Analytics) -> Bool {
-        
-        return false
-    }
-    
+
     public var target: CfTarget
     public var variation: Variation
     public var eventType: String
