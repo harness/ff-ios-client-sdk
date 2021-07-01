e2ab1d616267ab29169c5acb393e2cb852a7f2f7
Milos Vasic <milos.vasic@harness.io>
2021-07-01 12:23:22 +0200

    FFM-909 - Formatting.

--- Sources/ff-ios-client-sdk/API/CfConstants.swift
+++ Sources/ff-ios-client-sdk/API/CfConstants.swift
@@ -1,54 +1,50 @@
-//
-//  CfConstants.swift
-//  ff-ios-client-sdk
-//
-//  Created by Dusan Juranovic on 14.1.21..
-//
-
 import Foundation
 
-enum CFHTTPHeaderField: String
-{
-	case authorization  = "Authorization"
-	case contentType    = "Content-Type"
-	case acceptType     = "Accept"
-	case acceptEncoding = "Accept-Encoding"
-	case cacheControl	= "Cache-Control"
-	case apiKey			= "API-Key"
+enum CFHTTPHeaderField: String {
+    
+    case authorization  = "Authorization"
+    case contentType    = "Content-Type"
+    case acceptType     = "Accept"
+    case acceptEncoding = "Accept-Encoding"
+    case cacheControl	= "Cache-Control"
+    case apiKey			= "API-Key"
 }
 
-enum CFContentType: String
-{
-	case json = "application/json"
-	case form = "application/x-www-form-urlencoded"
-	case eventStream = "text/event-stream"
-	case noCache = "no-cache"
-	case textHtml = "text/html"
+enum CFContentType: String {
+    
+    case json = "application/json"
+    case form = "application/x-www-form-urlencoded"
+    case eventStream = "text/event-stream"
+    case noCache = "no-cache"
+    case textHtml = "text/html"
 }
 
-struct CfConstants
-{
-	enum Persistance {
-		case feature(String, String, String)
-		case features(String, String)
-		
-		var value: String {
-			switch self {
-				case let .feature(envId, target , feature): return "\(envId)_\(target)_\(feature)"
-				case let .features(envId, target): return "\(envId)_\(target)_features"
-			}
-		}
-	}
-	
+struct CfConstants {
+    
+    enum Persistance {
+    
+        case feature(String, String, String)
+        case features(String, String)
+        
+        var value: String {
+            
+            switch self {
+
+                case let .feature(envId, target , feature): return "\(envId)_\(target)_\(feature)"
+                case let .features(envId, target): return "\(envId)_\(target)_features"
+            }
+        }
+    }
+    
     struct Server {
-		
+        
         static let configUrl 	 = "https://config.ff.harness.io/api/1.0"
-		static let streamUrl 	 = "https://config.ff.harness.io/api/1.0/stream"
+        static let streamUrl 	 = "https://config.ff.harness.io/api/1.0/stream"
         static let eventUrl      = "https://events.ff.harness.io/api/1.0"
-	}
-	
-	struct ParamKey
-	{
-		static let authToken = "authToken"
-	}
+    }
+    
+    struct ParamKey {
+        
+        static let authToken = "authToken"
+    }
 }
