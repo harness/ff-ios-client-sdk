0b9b3a05b603e53ffbea6e6e337875194d7e96b4
Milos Vasic <milos.vasic@harness.io>
2021-08-12 11:47:44 +0200

    FFM-1004 - Connect wrapper classes with the test.

---
+++ test_wrapper/test_wrapper/WrapperServer.swift
@@ -0,0 +1,13 @@
+//
+//  WrapperServer.swift
+//  test_wrapper
+//
+//  Created by Milos Vasic on 12.8.21..
+//
+
+import Foundation
+
+class WrapperServer {
+    
+    // TODO
+}
--- test_wrapper/test_wrapperTests/test_wrapperTests.swift
+++ test_wrapper/test_wrapperTests/test_wrapperTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+
 @testable import test_wrapper
 
 class test_wrapperTests: XCTestCase {
@@ -22,18 +23,8 @@ class test_wrapperTests: XCTestCase {
 
     func testExample() throws {
         
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let server = WrapperServer()
         XCTAssert(1 == 1)
         NSLog("Test completed")
     }
-
-    func testPerformanceExample() throws {
-        
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
