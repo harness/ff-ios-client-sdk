6608b971661bbc05978730d47871d3bdf81b87b8
Andrew Bell <115623869+andybharness@users.noreply.github.com>
2025-04-28 15:54:54 +0100

    fix: [FFM-12394]: Fail-fast when we fail to get evaluations on init

--- Sources/ff-ios-client-sdk/Network/FeatureRepository.swift
+++ Sources/ff-ios-client-sdk/Network/FeatureRepository.swift
@@ -60,18 +60,11 @@ class FeatureRepository {
         self.config.environmentId, self.target.identifier
       ).value
       switch result {
-      case .failure(_):
-        FeatureRepository.logger.warn("Failed getting ALL from CLOUD. Try CACHE/STORAGE")
-        do {
-          let evals: [Evaluation]? = try self.storageSource.getValue(forKey: allKey)
-          onCompletion(.success(evals ?? []))
-          FeatureRepository.logger.debug("SUCCESS: Got ALL from CACHE/STORAGE")
-        } catch {
-          FeatureRepository.logger.warn("FAILURE: Unable to get ALL from CACHE/STORAGE")
-          onCompletion(.failure(CFError.storageError))
-        }
+      case .failure(let err):
+        FeatureRepository.logger.warn("Failed getting all evaluations from cloud")
+        onCompletion(.failure(err))
       case .success(let evaluations):
-        FeatureRepository.logger.debug("SUCCESS: Got ALL from CLOUD")
+        FeatureRepository.logger.debug("SUCCESS: Got all evaluations from cloud")
 
         for eval in evaluations {
           let key = CfConstants.Persistance.feature(
